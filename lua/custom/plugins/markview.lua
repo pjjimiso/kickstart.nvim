e875004d654ad427133a1cb1129b43e131eb5d80
Patrick Jimison <pat.jimison@gmail.com>
2026-07-30 09:21:48 -0700

    <leader>mt toggles markdown rendering

--- lua/custom/plugins/markview.lua
+++ lua/custom/plugins/markview.lua
@@ -1,9 +1,20 @@
 return {
-    'OXY2DEV/markview.nvim',
-    lazy = false,
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'echasnovski/mini.icons',
-    },
-    opts = {},
+  'OXY2DEV/markview.nvim',
+  lazy = false,
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'echasnovski/mini.icons',
+  },
+  opts = {},
+  config = function(_, opts)
+    require('markview').setup(opts)
+
+    -- Buffer-local keymap for toggling Markview in Normal mode
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'markdown' },
+      callback = function(event)
+        vim.keymap.set('n', '<leader>mt', '<cmd>Markview toggle<CR>', { buffer = event.buf, desc = '[M]arkdown [T]oggle' })
+      end,
+    })
+  end,
 }
